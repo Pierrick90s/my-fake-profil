638a3b148a36f34a8860c46cd0a35909161e6f09
Pierrick <pierrickgauvain@gmail.com>
2024-03-31 17:24:02 +0200

    ajout tables Utilisateurs, Villes, Pays et Groupes sans les relations

---
+++ src/Entity/Utilisateurs.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UtilisateursRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UtilisateursRepository::class)]
+class Utilisateurs
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 40)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 40)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $profession = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $intro = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $decrit = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date_naissance = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getProfession(): ?string
+    {
+        return $this->profession;
+    }
+
+    public function setProfession(string $profession): static
+    {
+        $this->profession = $profession;
+
+        return $this;
+    }
+
+    public function getIntro(): ?string
+    {
+        return $this->intro;
+    }
+
+    public function setIntro(string $intro): static
+    {
+        $this->intro = $intro;
+
+        return $this;
+    }
+
+    public function getDecrit(): ?string
+    {
+        return $this->decrit;
+    }
+
+    public function setDecrit(string $decrit): static
+    {
+        $this->decrit = $decrit;
+
+        return $this;
+    }
+
+    public function getDateNaissance(): ?\DateTimeInterface
+    {
+        return $this->date_naissance;
+    }
+
+    public function setDateNaissance(\DateTimeInterface $date_naissance): static
+    {
+        $this->date_naissance = $date_naissance;
+
+        return $this;
+    }
+}
