a4f93c3771544848eb6d475664f1bda0a6feaf40
Pierrick <pierrickgauvain@gmail.com>
2024-04-01 11:13:06 +0200

    test affichage objet Utilisateur, Villes, Pays et Groupes

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Groupes;
+use App\Entity\Pays;
+use App\Entity\Utilisateurs;
+use App\Entity\Villes;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,8 +15,27 @@ class MainController extends AbstractController
     #[Route('/', name: 'idex')]
     public function index(): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        $pays = new Pays();
+        $pays   ->setNom("France");
+
+        $villes = new Villes();
+        $villes ->setNom("Brest")
+                ->setPays($pays);
+
+        $groupes = new Groupes();
+        $groupes->setNom("Groupe A");
+
+        $utilisateurs = new Utilisateurs();
+        $utilisateurs   ->setNom("Gauvain")
+                        ->setPrenom("Pierrick")
+                        ->setDateNaissance(new \DateTimeImmutable)
+                        ->setEmail("pierrickgauvain@gmail.com")
+                        ->setVille($villes)
+                        ->setProfession("Web Developer")
+                        ->addGroupe($groupes)
+                        ->setIntro("Bonjour je m'appelle Pierrick Gauvain, 
+                                    j'ai 24ans et je suis en formation Web Developper.")
+                        ->setDecrit("J'aime le sport, les jeux vidéos, la musique et le cinéma.");
+                        dd($utilisateurs);
     }
 }
